e4705a4631af830b6ea3269d43bff45c83e75b22
taoshilai <taoshilai@x3platform.com>
2019-03-27 11:04:56 +0800

    reconstruct cnn

--- cnn_restruct.py
+++ cnn_restruct.py
@@ -42,61 +42,69 @@ fpaths, datas, labels = read_data(data_dir)
 
 # 计算有多少类图片
 num_classes = len(set(labels))
+graph=tf.Graph()
+with graph.as_default():
+
+    # 定义Placeholder，存放输入和标签
+    datas_placeholder = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    labels_placeholder = tf.placeholder(tf.int32, [None])
+
+    # 存放DropOut参数的容器，训练时为0.25，测试时为0
+    dropout_placeholdr = tf.placeholder(tf.float32)
+
+    # 定义卷积层, 20个卷积核, 卷积核大小为5，用Relu激活#卷积核是对所有通道进行卷积操作的
+    conv0 = tf.layers.conv2d(datas_placeholder, 20, 5, activation=tf.nn.relu)
+    # 定义max-pooling层，pooling窗口为2x2，步长为2x2
+    pool0 = tf.layers.max_pooling2d(conv0, [2, 2], [2, 2])
+
+    # 定义卷积层, 40个卷积核, 卷积核大小为4，用Relu激活
+    conv1 = tf.layers.conv2d(pool0, 40, 4, activation=tf.nn.relu)
+    # 定义max-pooling层，pooling窗口为2x2，步长为2x2
+    pool1 = tf.layers.max_pooling2d(conv1, [2, 2], [2, 2])
+
+    # 将3维特征转换为1维向量
+    flatten = tf.layers.flatten(pool1)
+
+    # 全连接层，转换为长度为100的特征向量
+    fc = tf.layers.dense(flatten, 400, activation=tf.nn.relu)
+
+    # 加上DropOut，防止过拟合
+    dropout_fc = tf.layers.dropout(fc, dropout_placeholdr)
+
+    # 未激活的输出层
+    logits = tf.layers.dense(dropout_fc, num_classes)
+
+    predicted_labels = tf.arg_max(logits, 1)
 
 
-# 定义Placeholder，存放输入和标签
-datas_placeholder = tf.placeholder(tf.float32, [None, 32, 32, 3])
-labels_placeholder = tf.placeholder(tf.int32, [None])
+    # 利用交叉熵定义损失
+    losses = tf.nn.softmax_cross_entropy_with_logits(
+        labels=tf.one_hot(labels_placeholder, num_classes),
+        logits=logits
+    )
+    # 平均损失
+    mean_loss = tf.reduce_mean(losses)
 
-# 存放DropOut参数的容器，训练时为0.25，测试时为0
-dropout_placeholdr = tf.placeholder(tf.float32)
-
-# 定义卷积层, 20个卷积核, 卷积核大小为5，用Relu激活#卷积核是对所有通道进行卷积操作的
-conv0 = tf.layers.conv2d(datas_placeholder, 20, 5, activation=tf.nn.relu)
-# 定义max-pooling层，pooling窗口为2x2，步长为2x2
-pool0 = tf.layers.max_pooling2d(conv0, [2, 2], [2, 2])
-
-# 定义卷积层, 40个卷积核, 卷积核大小为4，用Relu激活
-conv1 = tf.layers.conv2d(pool0, 40, 4, activation=tf.nn.relu)
-# 定义max-pooling层，pooling窗口为2x2，步长为2x2
-pool1 = tf.layers.max_pooling2d(conv1, [2, 2], [2, 2])
-
-# 将3维特征转换为1维向量
-flatten = tf.layers.flatten(pool1)
-
-# 全连接层，转换为长度为100的特征向量
-fc = tf.layers.dense(flatten, 400, activation=tf.nn.relu)
-
-# 加上DropOut，防止过拟合
-dropout_fc = tf.layers.dropout(fc, dropout_placeholdr)
-
-# 未激活的输出层
-logits = tf.layers.dense(dropout_fc, num_classes)
-
-predicted_labels = tf.arg_max(logits, 1)
+    # 定义优化器，指定要优化的损失函数
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(losses)
+    init = tf.initialize_all_variables()
 
 
-# 利用交叉熵定义损失
-losses = tf.nn.softmax_cross_entropy_with_logits(
-    labels=tf.one_hot(labels_placeholder, num_classes),
-    logits=logits
-)
-# 平均损失
-mean_loss = tf.reduce_mean(losses)
-
-# 定义优化器，指定要优化的损失函数
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(losses)
+#
+# print("logits: ", logits)
+# print("mean_loss: ", mean_loss)
 
 
-# 用于保存和载入模型
-saver = tf.train.Saver()
 
-with tf.Session() as sess:
-
+with tf.Session(
+        config=tf.ConfigProto(log_device_placement=True,allow_soft_placement=True),
+        graph=graph) as sess:
+    # 用于保存和载入模型#要放在session里面才可以
+    saver = tf.train.Saver()
     if train:
         print("训练模式")
         # 如果是训练，初始化参数
-        sess.run(tf.global_variables_initializer())
+        sess.run(init)
         # 定义输入和Label以填充容器，训练时dropout为0.25
         train_feed_dict = {
             datas_placeholder: datas,
@@ -135,7 +143,7 @@ with tf.Session() as sess:
             predicted_label_name = label_name_dict[predicted_label]
             print("{}\t{} => {}".format(fpath, real_label_name, predicted_label_name))
 
-
+#
 
 
 
